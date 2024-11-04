a27e1ceb4c1b304dad564d80e5a41b17b1b81756
Cherylshi127 <cherylshi127@gmail.com>
2024-11-04 15:53:08 -0500

    switched Fraciton to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 // import ${icons} from '../img/icon.svg'; // Parcel 1
 import View from './View.js';
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import { mark } from 'regenerator-runtime';
 
 class RecipeView extends View {
@@ -128,7 +128,7 @@ class RecipeView extends View {
           <use href="${icons} #icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
+          ing.quantity ? fracty(ing.quantity) : ''
         }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
